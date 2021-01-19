d864e6ec4e99fc3e3d29773e27b89ca03bc89afb
PatriziaCrimi <crimi.patrizia@gmail.com>
2021-01-19 17:24:29 +0100

    Controller created

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Fashion Shop</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    PROVA
+  </body>
+</html>
